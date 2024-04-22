50ff09c5540e52c4c3deedb2fd49cec69d34f61f
Abhishek Sur <contact@abhisheksur.com>
2024-04-22 10:05:29 +0530

    Update : Adding Redux Implementation

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,28 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
 import Product from '../components/Product'
-
+import Loader from '../components/Loader';
+import { useGetProductsQuery } from '../slices/productApiSlice';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            setProducts(data);
-        };
-
-        fetchProducts();
-    }, []);
+    const { data: products, isLoading, error } = useGetProductsQuery();
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
-            </Col>
-        ))}
-      </Row>
+     { isLoading ? (
+        <Loader />
+     ) : error ? (<div>{ error?.data?.message || error.error }</div>) : (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )}
+      
     </>
   )
 }
--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -1,13 +1,21 @@
-import { PRODUCTS_URL } from '../constants';
+import { PRODUCTS_URL } from '../constrants';
 import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        query: () => ({
-            url: PRODUCTS_URL
+        getProducts: builder.query({
+            query: () => ({
+                url: PRODUCTS_URL
+            }),
+            keepUnusedDataFor: 5
         }),
-        keepUnusedDataFor: 5
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5
+        })
     })
 });
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
